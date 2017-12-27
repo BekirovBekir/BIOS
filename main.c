95f40867c0b039f821717df020d3c2d29592fec9
bekir <bekir.bekirov@l1inc.com>
2017-12-27 15:57:59 +0200

    Touch screen is integrated

--- main.c
+++ main.c
@@ -19,29 +19,55 @@
 #include  "./inc/i2c.h"
 #include  "./inc/TS.h"
 
+#define DEBUG 1
 
+#if (DEBUG > 0)
+	#define DBG(x)	(x)
+#else
+	#define DBG(x)
+#endif
+
+pthread_t fsm_ts_thread;	//thread for timer
+int id_fsm_ts_thread=1;
+
+int timer_tick=0;
+
+void* fsm_ts_thread_func(void* thread_data)
+{
+static ilitek_key_info key={0,0,0,0};
+	for (;;)
+	{
+		key=Scan_TS_Key();
+		FSM_TS(&key);
+	}
+}
 
 int main(int argc, char* argv[])
 {
 	ilitek_key_info ts_key={0,0,0,0};
+
 	Open_i2c("/dev/i2c-2", 0x41); //setup
 
-	    /*unsigned char newbuf[31];
+	int trhread_state=pthread_create(&fsm_ts_thread, NULL, fsm_ts_thread_func, &id_fsm_ts_thread);
+		if (trhread_state==0)
+		{
+			printf ("\nFSM thread started\n");
+		}
+		else
+		{
+			perror("\nFSM thread fail\n");
+		}
 
-	    if(Read_i2c(0x10, newbuf, 31)>=0)
-	    {
-	    	printf("ILITEK: %i, %i, %i, %i, %i, %i, %i\n", newbuf[0], newbuf[1], newbuf[2], newbuf[3], newbuf[4], newbuf[5], newbuf[6]);
-	    }
-	    else
-	    {
-	    	printf("Fail\n");
-	    }*/
 
-	while (1)
+	while (getchar()!='q');
+	pthread_cancel(fsm_ts_thread);
+	pthread_join(fsm_ts_thread, NULL);
+
+	/*while (1)
 	{
 	ts_key=Scan_TS_Key();
 	usleep(300000);
-	}
+	}*/
 	Close_i2c(); //cleanup
 	return EXIT_SUCCESS;
 
--- src/TS.c
+++ src/TS.c
@@ -25,7 +25,7 @@ ilitek_key_info Scan_TS_Key(void)
 		for (unsigned int i=0; i<=4; i++)
 		{
 			key.key_num=-1;
-			key.status=-1;
+			key.status=0;
 			key.x=0;
 			key.y=0;
 			key.status=buf[(i*5)+1]>>7;
@@ -36,30 +36,57 @@ ilitek_key_info Scan_TS_Key(void)
 				{
 					if (key.y==0x25e4)
 					{
-						DBG(printf("key%i %i, %i, %i\n", i, key.status, key.x, key.y));
+						//DBG(printf("key%i %i, %i, %i\n", i, key.status, key.x, key.y));
 						return key;
 					}
 				}
 		}
 	key.key_num=-1;
-	key.status=-1;
+	key.status=0;
 	key.x=0;
 	key.y=0;
-	DBG(printf("key%i %i, %i, %i\n", -1, key.status, key.x, key.y));
+	//DBG(printf("key%i %i, %i, %i\n", -1, key.status, key.x, key.y));
 	return key;
 }
 
 // finite state machine for TS
 void FSM_TS (ilitek_key_info* key)
 {
-	unsigned char fsm=0;
+	static unsigned char fsm=0;
+	static ilitek_key_info _key={0,0,0,0};
+
 		switch (fsm)
 		{
 			case 0:
+				if (key->status)
+				{
+					fsm=1;
+					_key.key_num=key->key_num;
+					_key.status=key->status;
+					_key.x=key->x;
+					_key.y=key->y;
+					//DBG(printf("fsm - 0\n"));
+					usleep (30000);
 
+				}
 			break;
 			case 1:
-
+					if (((_key.status==key->status) && (_key.key_num==key->key_num)))
+					{
+						DBG(printf("key_pressed: %i\n", key->key_num));
+						fsm=2;
+					}
+					else
+					{
+						fsm=0;
+					}
+			break;
+			case 2:
+				if (((key->status==0) && (_key.key_num!=key->key_num)))
+				{
+					fsm=0;
+					//DBG(printf("fsm - 2\n"));
+				}
 			break;
 
 		}
