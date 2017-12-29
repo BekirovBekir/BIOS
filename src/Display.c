fa98e6e36e412e19318af869139c3725c63fc5a0
bekir <bekir.bekirov@l1inc.com>
2017-12-29 14:05:21 +0200

    Increased font 12x22

--- src/Display.c
+++ src/Display.c
@@ -42,22 +42,25 @@ void PreAsmDisp (void)
 	char cnt_byte;
 
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[22;60H \n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[17;41H \n\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[20;60H>1. Pre-Assembly Testing\n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[15;41H>1. Pre-Assembly Testing\n\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[60C2. Post-Assembly Testing\n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[41C2. Post-Assembly Testing\n\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[60C3. System Recovery\n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[41C3. System Recovery\n\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[60C4. Power Down to Shipping Mode\n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[41C4. Power Down to Shipping Mode\n\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[60C5. Exit - Boot OS\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[41C5. Exit - Boot OS\n");
+	write(fd_fb, buf, cnt_byte);
+	memset(buf, 0, 50);
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[36;0H");
 	write(fd_fb, buf, cnt_byte);
 }
 
@@ -77,13 +80,16 @@ void PostAsmDisp (void)
 	char cnt_byte;
 
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[24;60H \n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[19;41H \n\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[20;60H \n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[15;41H \n\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[59C>\n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[40C>\n\n");
+	write(fd_fb, buf, cnt_byte);
+	memset(buf, 0, 50);
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[36;0H");
 	write(fd_fb, buf, cnt_byte);
 
 }
@@ -94,13 +100,16 @@ void GIDisp (void)
 	char cnt_byte;
 
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[26;60H \n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[21;41H \n\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[22;60H \n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[17;41H \n\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[59C>\n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[40C>\n\n");
+	write(fd_fb, buf, cnt_byte);
+	memset(buf, 0, 50);
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[36;0H");
 	write(fd_fb, buf, cnt_byte);
 }
 
@@ -115,13 +124,16 @@ void ShipModeDisp (void)
 	char cnt_byte;
 
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[28;60H \n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[23;41H \n\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[24;60H \n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[19;41H \n\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[59C>\n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[40C>\n\n");
+	write(fd_fb, buf, cnt_byte);
+	memset(buf, 0, 50);
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[36;0H");
 	write(fd_fb, buf, cnt_byte);
 }
 
@@ -136,10 +148,13 @@ void ExitDisp (void)
 	char cnt_byte;
 
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[26;60H \n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[21;41H \n\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 50);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[59C>\n\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[40C>\n\n");
+	write(fd_fb, buf, cnt_byte);
+	memset(buf, 0, 50);
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[36;0H");
 	write(fd_fb, buf, cnt_byte);
 }
 
@@ -193,16 +208,22 @@ void MenuInit (void)
 	write(fd_fb, buf, cnt_byte);
 
 	memset(buf, 0, 200);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[17;40H***************************************************************\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[10;22H***************************************************************\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 200);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[18;40H*\x1b[19;40H*\x1b[20;40H*\x1b[21;40H*\x1b[22;40H*\x1b[23;40H*\x1b[24;40H*\x1b[25;40H*\x1b[26;40H*\x1b[27;40H*\x1b[28;40H*\x1b[29;40H*\x1b[30;40H*");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[11;42H=====BIOS PROGRAM=====\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 200);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[18;102H*\x1b[19;102H*\x1b[20;102H*\x1b[21;102H*\x1b[22;102H*\x1b[23;102H*\x1b[24;102H*\x1b[25;102H*\x1b[26;102H*\x1b[27;102H*\x1b[28;102H*\x1b[29;102H*\x1b[30;102H*");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[13;22H***************************************************************\n");
 	write(fd_fb, buf, cnt_byte);
 	memset(buf, 0, 200);
-	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[31;40H***************************************************************\n");
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[11;22H*\x1b[12;22H*\x1b[13;22H*\x1b[21;22H*\x1b[14;22H*\x1b[15;22H*\x1b[16;22H*\x1b[17;22H*\x1b[18;22H*\x1b[19;22H*\x1b[20;22H*\x1b[21;22H*\x1b[22;22H*\x1b[23;22H*\x1b[24;22H*\x1b[25;22H*");
+	write(fd_fb, buf, cnt_byte);
+	memset(buf, 0, 200);
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[11;84H*\x1b[12;84H*\x1b[13;84H*\x1b[14;84H*\x1b[15;84H*\x1b[16;84H*\x1b[17;84H*\x1b[18;84H*\x1b[19;84H*\x1b[20;84H*\x1b[21;84H*\x1b[22;84H*\x1b[23;84H*\x1b[24;84H*\x1b[25;84H*");
+	write(fd_fb, buf, cnt_byte);
+	memset(buf, 0, 200);
+	cnt_byte=snprintf(buf, sizeof(buf), "\x1b[26;22H***************************************************************\n");
 	write(fd_fb, buf, cnt_byte);
 	active_menu=&PreAsm;
 	active_menu->menudisplay();
